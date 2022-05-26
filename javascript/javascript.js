3bbb233fa4a465a72d56117f23002503cf447dd9
jthlesi <distanc87@gmail.com>
2022-05-26 17:51:50 +0900

    0526

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,31 +2,28 @@ $('.move').click(function (){
     	$('html, body').animate({scrollTop: $(this.hash).offset().top}, 400);
 });
 
-// 슬라이드
 
-// var idx = 0;
-// var slides = $('#rev_slide .rev_box').length;
-// var slidesWidth = $('#rev_slide').width();
-
-
-// function mov(num) {
-//   $('#rev_slide').style.left = -num * $('.rev_box').offsetWidth()+'px';
-//   idx = num;
-
-// };
-
-// function currentSlide() {
-//   if (idx < cou - 4) {
-
-//       mov(idx + 1);
-//   } else {
-//       mov(0);
-//   }
-// };
-$(function(){
-function slide(){
-  $('#rev_slide').style.left = '-500px';
-}
 
 setInterval(slide, 4000);
-})
+
+var idx = 0;
+
+ function slide(){
+   
+    $("#slide #rev_slide").animate({
+      "left":-440+"px"
+    },2000);
+    $(".dot").eq(idx+1).addClass("on").siblings().removeClass("on");
+
+    if(idx> $("#slide #rev_slide .rev_box").length-3){
+      $("#slide #rev_slide").animate({
+        "left":0
+      },1000);
+      idx=0;
+      $(this).addClass("on").siblings().removeClass("on");
+      
+    }
+    idx++;
+  }
+
+ 
