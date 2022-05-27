c715ad11524e339b7d06357a2f152baea8ecccc7
jthlesi <distanc87@gmail.com>
2022-05-27 19:30:06 +0900

    0527_1

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,8 +1,9 @@
+// 원페이지
 $('.move').click(function (){
     	$('html, body').animate({scrollTop: $(this.hash).offset().top}, 400);
 });
 
-
+//햄버거메뉴
 $("#small").click(function () {
   $("#navbar_small").addClass("open");
   if($('.ham').hasClass("fa-bars")){
@@ -13,34 +14,39 @@ $("#small").click(function () {
   $(".ham").removeClass("fa-times").addClass("fa-bars");}
 });
 
-
 window.onresize = function () {
   document.location.reload();
 };
 
 
+// 슬라이드
+var wrapWidth = document.querySelector('#slide').offsetWidth;
+var boxWidth = document.querySelectorAll('.rev_box')
+
+for(i=0;i<boxWidth.length;i++){
+boxWidth[i].style.width = wrapWidth / 3 +"px";
+}
 
 
-// setInterval(slide, 4000);
+var idx = 0;
+function slide() {
+  $("#slide #rev_slide").animate({
+    "left": - wrapWidth / 3 +"px"
+  }, 1500);
 
-// var idx = 0;
+  $(".dot").eq(idx+1).addClass("on").siblings().removeClass("on");
 
-//  function slide(){
-   
-//     $("#slide #rev_slide").animate({
-//       "left":-440+"px"
-//     },2000);
-//     $(".dot").eq(idx+1).addClass("on").siblings().removeClass("on");
+  idx++;
 
-//     if(idx> $("#slide #rev_slide .rev_box").length-3){
-//       $("#slide #rev_slide").animate({
-//         "left":0
-//       },1000);
-//       idx=0;
-//       $(this).addClass("on").siblings().removeClass("on");
-      
-//     }
-//     idx++;
-//   }
+  if (idx > $("#slide #rev_slide .rev_box").length - 3) {
+    $("#slide #rev_slide").animate({
+      "left": 0
+    }, 1500);
+    idx = 0;
+    $(".dot").eq(idx).addClass("on").siblings().removeClass("on");
+  }
+}
 
- 
+
+
+setInterval(slide, 3000);
